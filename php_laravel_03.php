57655b26ed4c7674b9ed831c39cf2666bf6ab0c1
谷口　大和 <taniguchiyamato@taniguchiyamato-no-MacBook-Air.local>
2018-12-21 22:59:58 +0900

    課題修正

--- php_laravel_03.php
+++ php_laravel_03.php
@@ -1,21 +1,25 @@
 <?php
+//課題１
+$name = "Taniguchi Yamato";
 if ($name = "Taniguchi Yamato"){
   echo "私は あなたの名前 です";
 } else {
   echo "あなたの名前ではありません";
 }
-
+echo "\n";
+//課題２
 for ($i = 1 ; $i <= 10000 ; $i++) {
   $total += $i;
 }
-echo $total
-
+echo $total;
+echo "\n";
+//課題３
 $fruits = array("Apple", "Banana", "Orange", "Pain", "Grape");
-foreach ($fruits as $fruits) {
-  echo "" . $fruits;
+foreach ($fruits as $fruits){
+  echo " ". $fruits;
   echo "\n";
 }
-
+//課題４
 $start =1;
 $end =100;
 for($i =$start; $i <=$end; $i++){

d1fe41e9e6359048e5797d75b7c2dd4c566b165f
谷口　大和 <taniguchiyamato@taniguchiyamato-no-MacBook-Air.local>
2018-12-27 19:28:12 +0900

    04

--- php_laravel_04.php
+++ php_laravel_04.php
@@ -1,1 +1,36 @@
-A
+<?php
+//課題１
+function sum($i){
+  $result = $i * 2;
+  echo $result;
+}
+sum(5);
+echo"\n";
+//課題２
+function f($a,$b){
+  $result = $a + $b;
+  echo $result;
+}
+f(2,3);
+echo"\n";
+//課題３
+function multiply($arr){
+  $result = 1;
+  foreach ($arr as $value){
+    $result *= $value;
+  }
+  return $result;
+}
+$c = array(1, 3, 5, 7, 9);
+echo multiply($c);
+echo "\n";
+//課題４
+function max_array($arr){
+  $max_number = $arr[0];
+  foreach($arr as $a){
+  }
+  return $max_number;
+}
+$a = array(1, 3, 5, 7, 9);
+echo $max_number;
+echo "\n";
